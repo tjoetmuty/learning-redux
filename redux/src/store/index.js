da80a93c67d07651d779f71fa376e9eb17838874
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-01-11 22:17:05 +0800

    create product slice

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slice/counterSlice";
-import todoReducer from "./slice/counterSlice"
+import todoReducer from "./slice/todoSlice"
+import productReducer from "./slice/counterSlice"
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    todo: todoReducer
+    todo: todoReducer,
+    product: productReducer,
   },
 });
